408b21b12a1d40fb4e52aeafdc4ab7c252052873
Vinicius Mendite <vinicius@sharbe.com.br>
2023-02-21 10:38:34 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import { styled } from '@/styles';
+import Image from 'next/image';
 
-const Title = styled('h1', {
-  color: '$green300',
-});
+import camiseta1 from '@/assets/camisetas/1.png';
+import camiseta2 from '@/assets/camisetas/2.png';
+
+import { HomeContainer, Product } from '@/styles/pages/home';
 
 export default function Home() {
-  return <Title>Hello World</Title>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -6,6 +6,7 @@ export const Container = styled('div', {
   alignItems: 'flex-start',
   justifyContent: 'center',
   minHeight: '100vh',
+  paddingBottom: '2rem',
 });
 
 export const Header = styled('header', {
