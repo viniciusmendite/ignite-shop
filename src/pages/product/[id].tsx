2027f2b22a9f1ec793af0188fdefee89b2637fd2
Vinicius Mendite <vinicius@sharbe.com.br>
2023-03-01 14:19:55 -0300

    feat: product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,26 @@
-import { useRouter } from 'next/router';
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product';
 
 export default function Product() {
-  const { query } = useRouter();
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
 
-  return <h1>Product {query.id}</h1>;
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis ad
+          sit aspernatur unde labore ipsum possimus earum quia explicabo.
+          Architecto ea magni consectetur eius! Nihil numquam perspiciatis quod
+          qui tenetur.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
